1b659ba903da355e683270c2af0befa1501a0b11
Suraj@786 <kumarniteshoffice786@gmail.com>
2022-09-07 13:11:03 +0530

    updated for prod

--- pages/_app.js
+++ pages/_app.js
@@ -338,7 +338,7 @@ function MyApp({ Component, pageProps }) {
         <></>
       )}
 
-      <Script src="js/jquery.min.js" />
+      {/* <Script src="js/jquery.min.js" />
 
       <Script src="js/popper.js" />
       <Script src="js/bootstrap.js" />
@@ -377,7 +377,7 @@ function MyApp({ Component, pageProps }) {
       <Script src="js/moment.min.js" />
       <Script src="js/moment-timezone.min.js" />
       <Script src="js/moment-timezone-with-data.min.js" />
-      <Script src="js/mine.js" />
+      <Script src="js/mine.js" /> */}
     </>
   );
 }
