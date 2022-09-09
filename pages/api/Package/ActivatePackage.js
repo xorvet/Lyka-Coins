9571a74f8a8e364f685a65c903742377bb4482d0
Suraj@786 <kumarniteshoffice786@gmail.com>
2022-09-09 21:47:26 +0530

    updated time

--- pages/api/Package/ActivatePackage.js
+++ pages/api/Package/ActivatePackage.js
@@ -22,7 +22,7 @@ const ActivatePackage = async (req, res) => {
   var date = today.getDate() + "/"+ parseInt(today.getMonth()+1) +"/"+ parseInt(today.getFullYear()+Number(PackagePeriod));
 
   var todayTime = new Date();
-  var time = parseInt(todayTime.getHours()+5+1) + ":" + todayTime.getMinutes()
+  var time = parseInt(todayTime.getHours()+5) + ":" + parseInt(todayTime.getMinutes()+34)
 
 
 
